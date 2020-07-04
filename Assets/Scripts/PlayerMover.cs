e60318de52e0a9abbfeaa4fe2c82bd1e9c2eff7c
Brandon Tom <magicink@gmail.com>
2020-07-04 11:33:34 -0700

    Implemented initial move script

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool _moving;
+    public iTween.EaseType easeType = iTween.EaseType.easeInOutExpo;
+    public float speed = 1.5f;
+
     void Start()
     {
-        
+        Move(new Vector3(3f, 0f, 0f));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 destination, float delay = 0.25f)
     {
-        
+        StartCoroutine(MoveRoutine(destination, delay));
+    }
+
+    private IEnumerator MoveRoutine(Vector3 destination, float delay)
+    {
+        _moving = true;
+
+        yield return new WaitForSeconds(delay);
+
+        iTween.MoveTo(gameObject, iTween.Hash(
+            "x", destination.x,
+            "y", destination.y,
+            "z", destination.z,
+            "easetype", easeType,
+            "speed", speed
+        ));
+
+        while (Vector3.Distance(transform.position, destination) > 0.01f)
+        {
+            yield return null;
+        }
+
+        transform.position = destination;
+        _moving = false;
     }
 }
