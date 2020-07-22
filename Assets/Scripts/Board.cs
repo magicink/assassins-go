8cca845775708ef09c09e5bb9ad52625e85492d3
Brandon Tom <magicink@gmail.com>
2020-07-22 07:21:04 -0700

    Created node game objects

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,13 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Board : MonoBehaviour
 {
     public static float Spacing = 2f;
-    public static readonly Vector2[] Directions = new Vector2[]
-    {
+
+    public static readonly Vector2[] Directions = {
         new Vector2(Spacing, 0f),
         new Vector2(-Spacing, 0f),
         new Vector2(0f, Spacing),
         new Vector2(0f, -Spacing), 
     };
+
+    private List<Node> _nodes  = new List<Node>();
+
+    public List<Node> Nodes => _nodes;
+
+    private void InitializeNodes()
+    {
+        _nodes = new List<Node>(FindObjectsOfType<Node>());
+    }
+
+    private void Awake()
+    {
+        InitializeNodes();
+    }
+
+    private void Start()
+    {
+        foreach (var node in Nodes)
+        {
+            node.Neighbors = Node.FindNeighbors(node, Nodes);
+        }
+    }
 }
