31dd3a18b6baa1845afdbb251d9a9a92ebcbe8a2
Brandon Tom <magicink@gmail.com>
2020-08-08 08:39:27 -0700

    assassins-go-19 Level Win

---
+++ Assets/Scripts/GrowEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GrowEffect : MonoBehaviour
+{
+    [SerializeField] private float time = 1.0f;
+    private void Start()
+    {
+        Grow();
+    }
+
+    private void Grow()
+    {
+        iTween.ScaleFrom(gameObject, iTween.Hash(
+            "scale", Vector3.zero,
+            "time", time));
+    }
+}
